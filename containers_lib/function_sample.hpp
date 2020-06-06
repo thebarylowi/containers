00aa698c25e2c51dfd9501659181ad85033195d3
thebarylowi <thebarylowi@gmial.com>
2020-06-06 15:20:23 +0200

    added new function and google test to practise

--- containers_lib/function_sample.hpp
+++ containers_lib/function_sample.hpp
@@ -3,3 +3,7 @@
 float add(float a, float b) {
 	return a + b;
 }
+
+float mult(float a, float b) {
+	return a * b;
+}
--- containers_lib_unittest/function_sample_test_suite.cpp
+++ containers_lib_unittest/function_sample_test_suite.cpp
@@ -4,3 +4,7 @@
 TEST(test_suite_example, should_add_two_digits) {
 	EXPECT_EQ(2, add(1, 1));
 }
+
+TEST(test_suite_example, should_multiply_two_digits) {
+	EXPECT_EQ(2, mult(2, 1));
+}
